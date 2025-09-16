7bfebd95eee0ff0d0072b6168c9257c1f4bc0f52
biswajeet <biswajeet.gupta@telgoo5.com>
2025-09-16 19:00:32 +0530

    chore- added parameters

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,91 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:vcare_payment_module/vcare_payment_module.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: VcarePaymentScreen());
-  }
-}
+    return const MaterialApp(
+      home: VcarePaymentView(
+        input: {
+          "client_supported_payment_sdk_config": {
+            "supports_multi_gateway": false,
+            "gateways": ["stripe"],
+            "stripe_configs": {
+              "secret_key":
+                  "sk_test_51S7YBkBwFOEeLOxUANMzOHjPL340krkd14AObmTKlDYyH3vbued2mL3xgPssA6LEE3bnIaEjXPTHHB99oS9Wuhpi00RRzzlgFc",
+              "publish_key":
+                  "pk_test_51S7YBkBwFOEeLOxUJATffi9xSWFFq0bwOSwYJro5wo7xPqEXX0AQ2Aaehk60SXiAwyy3VFLxgKKY87zjzBW188K300hqp5uYRL",
+            },
+          },
+          "client_name": "Flex Mobile",
+          "zip_code": "Flex Mobile",
 
-class PaymentScreen extends StatefulWidget {
-  const PaymentScreen({super.key});
-
-  @override
-  _PaymentScreenState createState() => _PaymentScreenState();
-}
-
-class _PaymentScreenState extends State<PaymentScreen> {
-  String? _result;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Set up listener for payment results
-    VcarePaymentModule.setPaymentResultListener((result) {
-      setState(() => _result = result);
-    });
-  }
-
-  // void _pay() async {
-  //   try {
-  //     // 1. Initialize Stripe
-  //     final init = await VcarePaymentModule.initGateway(
-  //       publishableKey:
-  //           '',
-  //       secretKey:
-  //           '', // TEST ONLY
-  //     );
-  //     setState(() => _result = "Init Gateway: $init");
-
-  //     // 2. Wait a tiny bit to ensure Activity is attached
-  //     await Future.delayed(const Duration(milliseconds: 100));
-
-  //     // 3. Start payment
-  //     await VcarePaymentModule.startPayment(
-  //       amount: 100, // 1 USD in cents
-  //       currency: 'usd',
-  //       publishableKey:
-  //           '',
-  //     );
-
-  //     // Result will be handled in the listener
-  //   } catch (e) {
-  //     setState(() => _result = 'Error: $e');
-  //   }
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Vcare Payment Example")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  // _pay();
-                });
-              },
-              child: const Text("Pay with Stripe"),
-            ),
-            const SizedBox(height: 20),
-            if (_result != null)
-              SelectableText(
-                _result!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 16),
-              ),
-          ],
-        ),
+        },
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
