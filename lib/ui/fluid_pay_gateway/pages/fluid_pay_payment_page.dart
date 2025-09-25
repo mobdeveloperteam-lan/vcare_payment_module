af7560160979d7980ee3736a64c7668600a736ef
Sandeep <sandeep.kumar1@vcaremail.com>
2025-09-25 19:22:23 +0530

    added fluid pay functions

--- lib/ui/fluid_pay_gateway/pages/fluid_pay_payment_page.dart
+++ lib/ui/fluid_pay_gateway/pages/fluid_pay_payment_page.dart
@@ -34,36 +34,32 @@ class _FluidPayPaymentGateWayPageState
   Widget build(BuildContext context) {
     final provider = Provider.of<FluidPayProvider>(context);
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("FluidPay Tokenizer")),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: provider.containerHeight,
-              child: FluidPayWebView(
-                key: _webViewKey,
-                apiKey: "pub_31uSkmo5HcVIZrvXUVUvVzUBrjS",
-                tokenizerUrl:
-                    "https://sandbox.fluidpay.com/tokenizer/tokenizer.js",
-              ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: provider.containerHeight,
+            child: FluidPayWebView(
+              key: _webViewKey,
+              apiKey: "pub_31uSkmo5HcVIZrvXUVUvVzUBrjS",
+              tokenizerUrl:
+                  "https://sandbox.fluidpay.com/tokenizer/tokenizer.js",
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                FluidPayHelper.submitPayment(); // global submit call
-              },
-              child: const Text("Submit Payment"),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "Token: ${provider.token}",
-              style:
-                  const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              FluidPayHelper.submitPayment(); // global submit call
+            },
+            child: const Text("Submit Payment"),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "Token: ${provider.token}",
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
